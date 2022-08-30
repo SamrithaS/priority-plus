674285e2d3c55baa78490213626a06d702a6587c
SamrithaS <samritha@timeless.co>
2022-08-30 18:21:16 +0530

    navigation example added

---
+++ www/scripts/demo.js
@@ -0,0 +1,44 @@
+
+function resizeable(elem) {
+  const $control = elem.querySelector(".js-resizeable__control");
+  const $target = elem.querySelector(".js-resizeable__target");
+
+  function updateTargetWidth(width = $control.value) {
+    $target.style.maxWidth = `${width}%`;
+  }
+
+  function handleOnControlChange() {
+    updateTargetWidth();
+  }
+
+  function setValue(newValue) {
+    $control.value = newValue;
+    $control.dispatchEvent(new Event("input"));
+  }
+
+  $control.addEventListener("input", handleOnControlChange);
+
+  return {
+    setValue,
+  };
+}
+
+Array.from(document.querySelectorAll(".js-p-target")).forEach((target) => {
+  priorityPlus(target, {
+    innerToggleTemplate: ({ totalCount, toggleCount }) => {
+      return toggleCount === totalCount
+        ? "Menu"
+        : `<span aria-label="More">More</span>`;
+    },
+  });
+});
+
+const resizeableInstances = Array.from(
+  document.querySelectorAll(".js-resizeable")
+).map(resizeable);
+
+window.matchMedia("(max-width: 60rem)").addListener(({ matches }) => {
+  if (matches) {
+    resizeableInstances.forEach((inst) => inst.setValue(100));
+  }
+});
